e3c45994208c6f562102351b650dfca41c241a77
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-03 10:51:14 -0400

    used resource generator to generate Startup, Category, User models, set up associations

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,4 +1,7 @@
 class Startup < ApplicationRecord
     belongs_to :category 
     belongs_to :user 
+    validates :boolean_field_name, inclusion: { in:[true,false]}
+    scope :disrupt_startups, -> { where(disrupt_startups: true)}
+    default_scope { where state: 'pending' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :startups
     has_many :categories, through: :startups 
+    validates :name, presence: {message: "can't be blank"}
+    validates :email, presence: true, uniqueness: true 
+    validates :password, confirmation: true, unless: -> { password.blank? }
+has_secure_password 
 end
