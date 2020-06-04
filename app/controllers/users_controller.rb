7f5038647f46b4d33e3d9bc2fd9bad423848dfc6
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-04 17:59:17 -0400

    created methods for UsersController class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ def home
     if session[:user_id]
         @user = User.find(session[:user_id])
     end
-    
+
 def new
     @user = User.new 
 end
@@ -19,21 +19,38 @@ def show
 end
 
 def create 
+    @user = User.new(user_params)
+    if @user.save 
+        redirect_to @user 
+    else 
+        redirect_to login_path
+    end
 end
 
 def edit
 end
 
 def update
+    @user = User.find_by(id: params[:id])
+    @user.update(user_params)
+    if @user.save 
+        redirect_to @user 
+    else
+        render :edit
+    end
 end
 
 def destroy 
+    @user = User.find_by(id: params[:id])
+    @user.destroy 
+    flash[:notice] = "User deleted."
+    redirect_to users_path
 end
 
-
-
-
-
+private
+def user_params
+    params.require(:user).permit(:name, :email, :password)
+end
 
 
 end
