1a6241ecd662c09ba524d4b7cce4724ebb0de4e8
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-04 17:04:05 -0400

    added before_action to application_controller and definded private methods for it, wrote skip_before_action in users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+before_action :require_login 
+before_action :find_user
+
+
+
+
+
+
+
+private 
+def require_login 
+    unless logged_in?
+        redirect_to new_login_url 
+    end
+end
+
+def find_user 
+    @user = User.find_by(id: params[:id])
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+skip_before_action :require_login, only: [:new, :create]
+skip_before_action :find_user, only: [:index, :new, :create]     
+def index
+    @users = User.all 
+end
+
+def new
+    @user = User.new 
+end
+
+def show 
+    
+end
+
+def create 
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy 
+end
+
+
+
+
+
+
+
 end
