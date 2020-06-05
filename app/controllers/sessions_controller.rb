129c9648152605fb40f44e4a05d535b2d1d353e9
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-05 18:27:31 -0400

    refactored SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,25 @@ class SessionsController < ApplicationController
     def new
     end
 
-    def create
-        @user = User.find_or_create_by(uid: auth['uid']) do |u|
-            #if user && user.authenticate(params[:password])
-            #session[:user_id] = user.id 
-                u.name = auth['info']['name']
+    def fb_create 
+        @user = User.find_or_create_by(uid: auth['info']['uid']) do |u
+            u.name = auth['info']['name']
             u.email = auth['info']['email']
             u.password = auth['info']['password']
+            u.save 
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        end
+
+    def create
+        |@user = User.find_or_create_by(username: params[:user][:username])
+            if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+            else 
+                render :new
+            end
+            
         end
 
         session[:user_id] = @user.id 
@@ -17,8 +29,8 @@ class SessionsController < ApplicationController
 
     def destroy
         reset_session 
-        #session.delete :user.id 
-        redirect_to login_path 
+        #session.delete :user_id 
+        redirect_to '/'  #login_path
     end
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
     resources :categories 
   end
 
+  root 'users#home'
+  
+  get '/auth/facebook/callback', to: "sessions#fb_create"
+  get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  post "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
