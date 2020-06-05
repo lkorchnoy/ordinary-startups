f7179b60af6ccc7b08dddf986a273695e60b1ce2
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-05 15:20:49 -0400

    set up views templates and crud actions in CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+    
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
 end
