38030e81335ccc508a8d2b7f384588778c27f6d8
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-11 16:40:46 -0400

    created startups/show.html

--- app/helpers/startups_helper.rb
+++ app/helpers/startups_helper.rb
@@ -1,5 +1,14 @@
 module StartupsHelper
-    def options_for_names
-        ['Artificial Intelligence', 'Robotics', 'Sensors', 'Superintelligence', 'Networks', 'Information Technology', 'Virtual Reality'].sort 
+
+    def category_id_field(startup)
+        if startup.category.nil? 
+            select_tag "startup[category_id]", options_from_collection_for_select(Category.all, :id, :name)
+        else  
+            hidden_field_tag 
+            "startup[category_id]", startup.category_id 
+        end 
     end
+
+    
+
 end
