6c743d97b8cdb5823f57af3f8d19a1a1ffbfebd2
lkorchnoy <lkorchnoy@yahoo.com>
2020-06-12 05:15:46 -0400

    added validations and error messages to users/new, startups/new, categories/new

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ApplicationRecord
     has_many :startups 
     has_many :users, through: :startups
-    
-    accepts_nested_attributes_for :startups 
+    validates :name, presence: true 
+    #accepts_nested_attributes_for :startups 
+
+    def startup_attributes=(startup)
+        self.startup = Startup.find_or_create_by(company: startup[:company])
+        self.startup.update(startup)
+    end 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,6 +1,8 @@
 class Startup < ApplicationRecord
     belongs_to :category 
     belongs_to :user 
+    validates :company, presence: true 
+    validates :innovation, presence: true 
     validates :product, inclusion: { in:[true,false]}
     scope :disrupt_startups, -> { where(disrupt_startups: true)}
     #default_scope { where state: 'pending' }
